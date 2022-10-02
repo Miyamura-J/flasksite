1d36c31a6bef71f9da0fdf0509f0acc4a840c4c4
JoyBoy <miyamura.maj@gmail.com>
2022-10-02 20:10:27 +0000

    Created the base.html template and rendered it in all routes

--- files/auth.py
+++ files/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "<h1>Login</h1>"
+    return render_template("base.html")
 
 @auth.route("/logout")
 def logout():
-    return "<h1>Logout</h1>"
+    return render_template("base.html")
 
 @auth.route("/signup")
 def sign_up():
-    return "<h1>Sign Up</h1>"
+    return render_template("base.html")
