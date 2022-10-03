686b4a8fc9cb10773cad399f838d175b129fa9a9
JoyBoy <miyamura.maj@gmail.com>
2022-10-03 13:39:34 +0000

    Get data from sign up post request

--- files/auth.py
+++ files/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
@@ -10,6 +10,24 @@ def login():
 def logout():
     return render_template("home.html")
 
-@auth.route("/sign_up")
+@auth.route("/sign_up", methods=["GET","POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        fullName = request.form.get("name")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        valid = True
+        if len(email) < 4:
+            valid = False
+        if len(fullName) < 2:
+            valid = False
+        if password1 != password2:
+            valid = False
+        if len(password1) < 8:
+            valid = False
+        if valid:
+            # add to db
+            pass
+
     return render_template("sign_up.html")
