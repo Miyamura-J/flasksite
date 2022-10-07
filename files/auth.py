c43e84783415af87f54e307497c23cd1aef3927d
JoyBoy <miyamura.maj@gmail.com>
2022-10-07 18:16:45 +0000

    Using loginManager to login, or logout users

--- files/auth.py
+++ files/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint("auth", __name__)
 
@@ -16,6 +17,7 @@ def login():
         if user:
             if check_password_hash(user.password,password):
                 flash("Loged in succesfuly", category="success")
+                login_user(user, remember=True)
                 return redirect(url_for("views.home"))
             else:
                 flash("Wrong password", category="error")
@@ -24,8 +26,10 @@ def login():
     return render_template("login.html")
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return render_template("home.html")
+    logout_user()
+    return redirect(url_for("auth.login"))
 
 @auth.route("/sign_up", methods=["GET","POST"])
 def sign_up():
@@ -56,6 +60,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash("Account created", category="success")
+            login_user(user, remember=True)
             return redirect(url_for("views.home"))
 
     return render_template("sign_up.html")
