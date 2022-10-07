3c9f32c6f379d442aa56d2f64467ff059aa4a9a5
JoyBoy <miyamura.maj@gmail.com>
2022-10-07 13:33:17 +0000

    Syntax error fix

--- files/models.py
+++ files/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Comumn(db.String(150), unique=True)
+    email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     name = db.Column(db.String(150))
     notes = db.relationship('Note')
